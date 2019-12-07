32b12b0f7cd9d7a5491074146d8310606fa1c51e
iMaksimkin <int-8@yandex.ru>
2019-12-07 16:14:20 +0400

    Merge branch 'devlop' of C:\Users\ilya.maksimkin\Documents\temp\CDV with conflicts.

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -8,7 +8,8 @@ public class Run {
         EmployeeInfoParser employeeInfoParser = new EmployeeInfoParser();
 
         employeeInfoParser.readCredsfromconsole();
-        List userEmailsList = employeeInfoParser.getUsersEmail();
+        List userEmailsList = employeeInfoParser.getUsersEmail(0);
+
        List userPagesList = employeeInfoParser.getAccountInfoResponse(userEmailsList);
         employeeInfoParser.getAccountInfo(userPagesList);
 
